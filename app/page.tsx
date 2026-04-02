763b119c3b3840be294ec42b13206b45fc47051e
shahshanka roy <30293164+shashankaroy5269@users.noreply.github.com>
2026-04-02 12:56:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Page() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>🛒 Grocery App</h1>
+      <h1 className={styles.title}> Grocery App</h1>
 
       <div className={styles.controls}>
         <input
